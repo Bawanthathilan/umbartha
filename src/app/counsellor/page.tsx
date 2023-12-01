02beb1b824f8be240f294a81e6022c8a984fa784
Bawanthathilan <bawantharathnayaka@gmail.com>
2023-12-01 21:58:46 +0530

    fix herosection issue

---
+++ src/app/counsellor/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import HeroSection from '@/components/common/HeroSection'
+
+const page = () => {
+    return (
+        <div>
+            <HeroSection
+                subtitle='Counsellor'
+                title='Meet our Counsellor'
+                left={true}
+            />
+        </div>
+    )
+}
+
+export default page
--- src/components/common/HeroSection/index.tsx
+++ src/components/common/HeroSection/index.tsx
@@ -3,22 +3,24 @@ import React from 'react'
 interface HeroSectionProps {
     subtitle: string,
     title: string,
-    desc?: string
+    desc?: string,
+    left?: boolean
 }
 
 const HeroSection = ({
     subtitle,
     title,
-    desc
+    desc,
+    left = false
 }: HeroSectionProps) => {
     return (
         <div className=' h-[520px] md:h-[400px] lg:h-[529px] w-full bg-hero-gradient px-5'>
-            <div className="cont flex w-full h-full flex-col items-left lg:items-center lg:justify-center md:justify-end justify-end gap-4">
+            <div className={`container mx-auto max-w-6xl flex w-full h-full flex-col items-left ${left ? "lg:items-start" : "lg:items-center"}  lg:justify-center md:justify-end justify-end gap-4`}>
                 <h4 className=' uppercase text-xl font-bold text-[#014840]'>{subtitle}</h4>
                 <h1 className=' text-[40px] lg:text-[52px] font-bold text-black max-w-2xl text-left lg:text-center'>{title}</h1>
                 <p className=' text-[20px] font-normal text-left lg:text-center max-w-6xl'>{desc}</p>
             </div>
-        </div>
+        </div >
     )
 }
 
