8c60dbd8a493302fb7800783d4f347156bfa88fc
Bawanthathilan <bawantharathnayaka@gmail.com>
2023-12-01 12:24:58 +0530

    about us page mobile responsive

--- src/components/about/about-section/index.tsx
+++ src/components/about/about-section/index.tsx
@@ -10,34 +10,34 @@ import CustomButton from '@/components/common/button'
 
 const AboutSection = () => {
     return (
-        <div className='bg-[#F9F6F3] pt-[68px]'>
-            <div className="container mx-auto max-w-7xl">
+        <div className='bg-[#F9F6F3] pt-[20px] lg:pt-[68px]'>
+            <div className="container mx-auto max-w-7xl px-5 lg:px-0">
                 <div className="section-1 flex flex-col gap-10">
-                    <h1 className=' text-[42px] text-[#082623] font-semibold leading-[50px]'>Our solar panels are designed to be highly efficient, delivering maximum solar production year-round, even on roofs with complicated angles.</h1>
-                    <p className='text-[#4E4E4E] text-xl font-normal'>Umbartha, a Marathi word meaning ‘threshold’ was originally chosen as the name for a business venture that Manisha Billimoria started in 2014 in a small way with exhibitions and sales of authentic Indian clothing such as sarees sourced directly from the looms of rural Bengal and Gujarat as well as Indian jewelry handicrafted by local Indian artisans as well as cottage industries in Sri Lanka. This was the crossing of a threshold for Manisha who had arrived as a young bride in Sri Lanka in 1996 having had her own fashion designing career in Pune.</p>
+                    <h1 className=' text-xl lg:text-[42px] text-[#082623] font-semibold leading-normal lg:leading-[50px]'>Our solar panels are designed to be highly efficient, delivering maximum solar production year-round, even on roofs with complicated angles.</h1>
+                    <p className='text-[#4E4E4E] text-base lg:text-xl font-normal'>Umbartha, a Marathi word meaning ‘threshold’ was originally chosen as the name for a business venture that Manisha Billimoria started in 2014 in a small way with exhibitions and sales of authentic Indian clothing such as sarees sourced directly from the looms of rural Bengal and Gujarat as well as Indian jewelry handicrafted by local Indian artisans as well as cottage industries in Sri Lanka. This was the crossing of a threshold for Manisha who had arrived as a young bride in Sri Lanka in 1996 having had her own fashion designing career in Pune.</p>
                 </div>
 
-                <div className="secton-2 my-10">
+                <div className="secton-2 my-10 ">
                     <div className="container mx-auto max-w-7xl">
-                        <div className="grid grid-cols-2 items-baseline gap-5">
+                        <div className="grid grid-cols-2 items-baseline gap-3 lg:gap-5">
                             <div className="itemOne flex flex-col gap-5">
-                                <div className="img relative h-[587px] w-full">
-                                    <Image src={Yoga} alt='yoga' fill className=' rounded-xl' />
+                                <div className="img relative h-[367px] lg:h-[587px] w-full">
+                                    <Image src={Yoga} alt='yoga' fill className=' object-cover rounded-xl' />
                                 </div>
 
                                 <div className="details flex flex-col">
-                                    <h4 className=' text-[28px] font-semibold'>Vision</h4>
-                                    <p className='text-[#4E4E4E] font-normal text-xl'>Our vision is to support with a listening ear and an empathetic heart in an integrated and relational counseling practise.</p>
+                                    <h4 className=' text-xl lg:text-[28px] font-semibold'>Vision</h4>
+                                    <p className='text-[#4E4E4E] font-normal text-base lg:text-xl'>Our vision is to support with a listening ear and an empathetic heart in an integrated and relational counseling practise.</p>
                                 </div>
                             </div>
 
                             <div className="itemTwo flex flex-col gap-5">
-                                <div className="img relative h-[443px] w-full">
-                                    <Image src={Write} alt='yoga' fill className=' rounded-xl' />
+                                <div className="img relative h-[281px] lg:h-[443px] w-full">
+                                    <Image src={Write} alt='yoga' fill className=' rounded-xl object-cover' />
                                 </div>
                                 <div className="details flex flex-col">
-                                    <h4 className=' text-[28px] font-semibold'>Mission</h4>
-                                    <p className='text-[#4E4E4E] font-normal text-xl'>Our mission is to serve the community by bringing healing and hope to individuals, families, couples, and others seeking emotional and relational wellbeing as well as wholesome interpersonal relationships through professional counselling care.</p>
+                                    <h4 className=' text-xl lg:text-[28px] font-semibold'>Mission</h4>
+                                    <p className='text-[#4E4E4E] font-normal text-base lg:text-xl'>Our mission is to serve the community by bringing healing and hope to individuals, families, couples, and others seeking emotional and relational wellbeing as well as wholesome interpersonal relationships through professional counselling care.</p>
                                 </div>
                             </div>
                         </div>
@@ -45,19 +45,19 @@ const AboutSection = () => {
                 </div>
 
                 <div className="section-3">
-                    <div className="grid grid-cols-2 items-center">
+                    <div className="grid grid-cols-1 lg:grid-cols-2 items-center gap-5 lg:gap-0 text-center lg:text-left">
                         <div className="manisha">
                             <Image src={manisha} alt='manisha' width={458} height={574} className='object-cover' />
                         </div>
 
                         <div className="right flex flex-col gap-5">
                             <div className="title flex flex-col text-[#26AF9F] gap-5">
-                                <h4 className=' text-[40px] font-semibold'>Manisha Billimoria</h4>
+                                <h4 className=' text-2xl lg:text-[40px] font-semibold'>Manisha Billimoria</h4>
                                 <p className='uppercase font-normal text-lg'>Counselor & Therapist at Umbartha Beyond Thresholds</p>
                             </div>
-                            <p className='text-[#4E4E4E] font-normal text-lg text-left'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt  labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore e.</p>
+                            <p className='text-[#4E4E4E] font-normal text-base lg:text-lg text-left'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt  labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore e.</p>
 
-                            <div className="btn-group flex flex-row gap-10 items-center">
+                            <div className="btn-group flex flex-col lg:flex-row gap-10 items-center">
                                 <CustomButton text="Make an Appointment" />
 
                                 <Image src={Sig} alt='signature' width={228} height={141} />
@@ -72,7 +72,7 @@ const AboutSection = () => {
 
             </div>
             <div className="strip bg-[#F1EFEC] py-[45px]">
-                <div className="container mx-auto max-w-7xl flex flex-row justify-between">
+                <div className="container mx-auto max-w-7xl grid grid-cols-2 gap-5 lg:gap-0 lg:flex lg:flex-row justify-between">
                     <div className="number flex flex-col gap-3 items-center">
                         <h1 className='text-[#595959] text-7xl font-semibold'>18</h1>
                         <h4 className='text-blac text-lg font-medium'>Award Winner</h4>
--- src/components/common/HeroSection/index.tsx
+++ src/components/common/HeroSection/index.tsx
@@ -10,10 +10,10 @@ const HeroSection = ({
     title
 }: HeroSectionProps) => {
     return (
-        <div className='h-[529px] w-full bg-hero-gradient'>
-            <div className="cont flex w-full h-full flex-col items-center justify-center gap-4">
+        <div className=' h-[520px] lg:h-[529px] w-full bg-hero-gradient px-5'>
+            <div className="cont flex w-full h-full flex-col items-left lg:items-center justify-center gap-4">
                 <h4 className=' uppercase text-xl font-bold text-[#014840]'>{subtitle}</h4>
-                <h1 className=' text-[52px] font-bold text-black max-w-2xl text-center'>{title}</h1>
+                <h1 className='text-[52px] font-bold text-black max-w-2xl text-left lg:text-center'>{title}</h1>
             </div>
         </div>
     )
