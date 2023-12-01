7a2d77abf60ea26260db22024720ada1e14724eb
Bawanthathilan <bawantharathnayaka@gmail.com>
2023-12-01 19:19:33 +0530

    add healing page

--- src/app/healing/page.tsx
+++ src/app/healing/page.tsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import HeroSectionImg from '@/components/common/HeroSectionImg'
-import Bgimg from '@/assets/home/inquery/1.png'
+
+import Bgimg from '@/assets/home/healing/bg.png'
+
 import Image from 'next/image'
-import manisha from '@/assets/home/inquery/2.png'
+
+import manisha from '@/assets/home/aboutus/manisha.png'
 import Sig from '@/assets/home/aboutus/manishasig.png'
+
 import CustomButton from '@/components/common/button'
 
 const page = () => {
     return (
         <div>
             <HeroSectionImg
-                title='Compassionate Inquiry By Dr. Gabor Mate.'
+                title='Trauma Healing'
                 img={Bgimg}
             />
 
@@ -20,22 +24,23 @@ const page = () => {
                 </div>
 
                 <div className="section-3 container mx-auto max-w-7xl mt-10 lg:mt-[80px]">
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 items-center gap-5 lg:gap-10 text-center  lg:text-left">
-                        <div className="manisha relative w-auto lg:h-full h-[400px]">
-                            <Image src={manisha} alt='manisha' fill className='object-contain object-center lg:object-left' />
-                        </div>
-
-                        <div className="right flex flex-col gap-5">
-                            <div className="title flex flex-col  gap-5">
-                                <h4 className=' text-2xl lg:text-[40px] text-[#26AF9F] font-semibold'>Dr. Gabor Maté</h4>
-                                <p className='uppercase font-normal text-lg'>Executive Director</p>
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 items-center gap-5 lg:gap-10 text-center lg:text-left">
+                        <div className="right flex flex-col gap-5 order-2 lg:order-1">
+                            <div className="title flex flex-col gap-5">
+                                <h4 className=' text-2xl lg:text-[40px] text-[#26AF9F] font-semibold'>Manisha Billimoria</h4>
+                                <p className='uppercase font-normal text-lg'>Counselor & Therapist at Umbartha Beyond Thresholds</p>
                             </div>
                             <p className='text-[#4E4E4E] font-normal text-base lg:text-lg text-center lg:text-left'>A renowned speaker, and bestselling author, Dr. Gabor Maté is highly sought after for his expertise on a range of topics including addiction, stress and childhood development.</p>
 
                             <div className="btn-group flex flex-col lg:flex-row gap-10 items-center">
                                 <CustomButton text="Make an Appointment" />
+                                <Image src={Sig} alt='signature' width={228} height={141} />
                             </div>
 
+                        </div>
+
+                        <div className="manisha relative w-auto  h-[400px] lg:h-full flex items-end order-1 lg:order-2">
+                            <Image src={manisha} alt='manisha' fill className='object-contain lg:object-right object-center' />
                         </div>
                     </div>
 
--- src/components/common/HeroSectionImg/index.tsx
+++ src/components/common/HeroSectionImg/index.tsx
@@ -16,12 +16,12 @@ const HeroSectionImg = ({
 
 }: HeroSectionImgProps) => {
     return (
-        <div className=' h-[520px] lg:h-[529px] w-full relative'>
+        <div className=' h-[520px] lg:h-[529px] w-full relative px-5 lg:px-0'>
             <Image src={img} alt="bg" fill className=' object-cover -z-10' />
             <div className='bg-hero-gradient-2 w-full h-full absolute top-0' />
 
-            <div className="container max-w-7xl flex w-full h-full flex-col items-left lg:items-center lg:justify-end justify-end gap-4 z-50 pb-10">
-                <h1 className='text-[52px] font-bold text-white max-w-2xl text-left lg:text-left'>{title}</h1>
+            <div className="container mx-auto max-w-7xl flex w-full h-full flex-col items-left lg:items-left lg:justify-end justify-end gap-4 z-50 pb-10">
+                <h1 className=' text-[30px] lg:text-[52px] font-bold text-white max-w-2xl text-left lg:text-left'>{title}</h1>
             </div>
 
 
