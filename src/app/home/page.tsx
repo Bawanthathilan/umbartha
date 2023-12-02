204a43e45e1e629cfde5398ffbb0c6f27f38c374
Bawanthathilan <bawantharathnayaka@gmail.com>
2023-12-02 10:55:24 +0530

    responsivness

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -12,8 +12,8 @@ const HomePage = () => {
       <div className="bg-bg-image bg-cover">
         <div className="relative bg-cover bg-center text-center text-white">
           <div className=" h-screen flex justify-center items-center">
-            <div className="hero-text max-w-sm lg:max-w-7xl">
-              <h1 className=" text-[40px] lg:text-[64px] text-left lg:text-center font-bold leading-normal">A Dedicated Team <br /> committed to supporting <br /> with a listening ear </h1>
+            <div className="hero-text max-w-sm lg:max-w-7xl flex flex-col items-start lg:items-center px-10 md:px-0 lg:px-0">
+              <h1 className=" text-[40px] lg:text-[64px] text-left lg:text-center font-bold leading-normal ">A Dedicated Team <br /> committed to supporting <br /> with a listening ear </h1>
               <button className="bg-white mt-8 py-[14px] px-[51px] border-none text-[#082623] text-base">Learn More</button>
             </div>
           </div>
--- src/components/home-section/What-we-offer/index.tsx
+++ src/components/home-section/What-we-offer/index.tsx
@@ -45,8 +45,8 @@ const WhatWeOffer = () => {
                                 spaceBetween: 20,
                             },
                             768: {
-                                slidesPerView: 4,
-                                spaceBetween: 40,
+                                slidesPerView: 2,
+                                spaceBetween: 10,
                             },
                             1024: {
                                 slidesPerView: 4,
