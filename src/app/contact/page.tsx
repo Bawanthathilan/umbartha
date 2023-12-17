da8d0e07be4289f8a5a6c4bf21eb37ce1b8c866a
Bawanthathilan <bawantharathnayaka@gmail.com>
2023-12-17 21:49:43 +0530

    fixed contact page issues

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -67,7 +67,7 @@ const page = () => {
 
       <div className="bg-bg-white-varient pb-14 ">
         <div className="container mx-auto max-w-6xl px-5 lg:px-0 relative z-10">
-          <div className="grid grid-cols-1 lg:grid-cols-2 gap-10">
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-[80px]">
             <div className="left flex flex-col gap-5 order-2 lg:order-1">
               <div className="title">
                 <h1 className=" text-xl lg:text-4xl font-bold">Drop a Line</h1>
@@ -75,7 +75,7 @@ const page = () => {
 
               <form action="" className="flex flex-col gap-5">
                 <input
-                  className="px-5 py-3 rounded-3xl w-full"
+                  className="px-5 py-3 rounded-3xl w-full border-[1.5] border-[#ECECEC] placeholder:text-black placeholder:text-sm"
                   type="text"
                   name=""
                   id=""
@@ -84,14 +84,14 @@ const page = () => {
 
                 <div className="flex flex-col lg:flex-row gap-5">
                   <input
-                    className="px-5 py-3 rounded-3xl w-full"
+                    className="px-5 py-3 rounded-3xl w-full border-[1.5] border-[#ECECEC] placeholder:text-black placeholder:text-sm"
                     type="text"
                     name=""
                     id=""
                     placeholder="Your Email"
                   />
                   <input
-                    className="px-5 py-3 rounded-3xl w-full"
+                    className="px-5 py-3 rounded-3xl w-full border-[1.5] border-[#ECECEC] placeholder:text-black placeholder:text-sm"
                     type="text"
                     name=""
                     id=""
@@ -100,8 +100,9 @@ const page = () => {
                 </div>
 
                 <textarea
-                  className="px-5 py-3 rounded-3xl w-full"
+                  className="px-5 py-3 rounded-3xl w-full border-[1.5] border-[#ECECEC] placeholder:text-black placeholder:text-sm"
                   name=""
+                  placeholder="Your comment"
                   id=""
                   cols={30}
                   rows={10}
@@ -137,12 +138,72 @@ const page = () => {
               </div>
 
               <div className="flex flex-col gap-5 text-base lg:text-[20px] font-semibold">
-                <h4>No 37/1, Hotel Road, Mount Lavinia, Sri Lanka</h4>
-                <h4>manisha@umbartha.org</h4>
-                <h4>+34 77 84 95 758</h4>
+                <div className="flex flex-row items-center gap-5">
+                  <div className="icon bg-white p-3 rounded-full">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                    >
+                      <path
+                        d="M12 11.5C11.337 11.5 10.7011 11.2366 10.2322 10.7678C9.76339 10.2989 9.5 9.66304 9.5 9C9.5 8.33696 9.76339 7.70107 10.2322 7.23223C10.7011 6.76339 11.337 6.5 12 6.5C12.663 6.5 13.2989 6.76339 13.7678 7.23223C14.2366 7.70107 14.5 8.33696 14.5 9C14.5 9.3283 14.4353 9.65339 14.3097 9.95671C14.1841 10.26 13.9999 10.5356 13.7678 10.7678C13.5356 10.9999 13.26 11.1841 12.9567 11.3097C12.6534 11.4353 12.3283 11.5 12 11.5ZM12 2C10.1435 2 8.36301 2.7375 7.05025 4.05025C5.7375 5.36301 5 7.14348 5 9C5 14.25 12 22 12 22C12 22 19 14.25 19 9C19 7.14348 18.2625 5.36301 16.9497 4.05025C15.637 2.7375 13.8565 2 12 2Z"
+                        fill="black"
+                      />
+                    </svg>
+                  </div>
+                  <h4 className=" text-base font-medium">
+                    No 37/1, Hotel Road, Mount Lavinia, Sri Lanka
+                  </h4>
+                </div>
+
+                <div className="flex flex-row items-center gap-5">
+                  <div className="icon bg-white p-3 rounded-full">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                    >
+                      <path
+                        d="M20 8L12 13L4 8V6L12 11L20 6M20 4H4C2.89 4 2 4.89 2 6V18C2 18.5304 2.21071 19.0391 2.58579 19.4142C2.96086 19.7893 3.46957 20 4 20H20C20.5304 20 21.0391 19.7893 21.4142 19.4142C21.7893 19.0391 22 18.5304 22 18V6C22 4.89 21.1 4 20 4Z"
+                        fill="black"
+                      />
+                    </svg>
+                  </div>
+                  <h4>manisha@umbartha.org</h4>
+                </div>
+
+                <div className="flex flex-row items-center gap-5">
+                  <div className="icon bg-white p-3 rounded-full">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                    >
+                      <path
+                        d="M6.62 10.79C8.06 13.62 10.38 15.94 13.21 17.38L15.41 15.18C15.69 14.9 16.08 14.82 16.43 14.93C17.55 15.3 18.75 15.5 20 15.5C20.2652 15.5 20.5196 15.6054 20.7071 15.7929C20.8946 15.9804 21 16.2348 21 16.5V20C21 20.2652 20.8946 20.5196 20.7071 20.7071C20.5196 20.8946 20.2652 21 20 21C15.4913 21 11.1673 19.2089 7.97918 16.0208C4.79107 12.8327 3 8.50868 3 4C3 3.73478 3.10536 3.48043 3.29289 3.29289C3.48043 3.10536 3.73478 3 4 3H7.5C7.76522 3 8.01957 3.10536 8.20711 3.29289C8.39464 3.48043 8.5 3.73478 8.5 4C8.5 5.25 8.7 6.45 9.07 7.57C9.18 7.92 9.1 8.31 8.82 8.59L6.62 10.79Z"
+                        fill="black"
+                      />
+                    </svg>
+                  </div>
+                  <h4>+34 77 84 95 758</h4>
+                </div>
               </div>
 
-              <Image src={Map} alt="map" className="rounded-lg  object-cover" />
+              <div className=" rounded-lg w-full">
+                <iframe
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31686.49176203425!2d79.83062063234242!3d6.913131607518933!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae25960d359dbbf%3A0xd64947e9ab7a4c4f!2sColombo%2003%2C%20Colombo!5e0!3m2!1sen!2slk!4v1702829758977!5m2!1sen!2slk"
+                  className="w-full h-[205px] rounded-lg"
+                  style={{ border: 0 }}
+                  loading="lazy"
+                  referrerPolicy="no-referrer-when-downgrade"
+                ></iframe>
+              </div>
             </div>
           </div>
 
--- src/components/common/HeroSectionImg/index.tsx
+++ src/components/common/HeroSectionImg/index.tsx
@@ -1,32 +1,28 @@
-import React from 'react'
-import Image from 'next/image'
-
-
+import React from "react";
+import Image from "next/image";
 
 interface HeroSectionImgProps {
-    img?: any
-    title?: string,
-
+  img?: any;
+  title?: string;
 }
 
 const HeroSectionImg = ({
-    img,
+  img,
 
-    title,
-
+  title,
 }: HeroSectionImgProps) => {
-    return (
-        <div className=' h-[520px] lg:h-[529px] w-full relative px-5 lg:px-0'>
-            <Image src={img} alt="bg" fill className=' object-cover -z-10' />
-            <div className='bg-hero-gradient-2 w-full h-full absolute top-0' />
+  return (
+    <div className=" h-[520px] lg:h-[529px] w-full relative px-5 lg:px-0">
+      <Image src={img} alt="bg" fill className=" object-cover -z-10" />
+      <div className="bg-hero-gradient-2 w-full h-full absolute top-0" />
 
-            <div className="container mx-auto max-w-7xl flex w-full h-full flex-col items-left lg:items-left lg:justify-end justify-end gap-4 z-50 pb-10">
-                <h1 className=' text-[30px] lg:text-[52px] font-bold text-white max-w-2xl text-left lg:text-left'>{title}</h1>
-            </div>
+      <div className="container mx-auto max-w-7xl flex w-full h-full flex-col items-left lg:items-left lg:justify-end justify-end gap-4 z-50 pb-10">
+        <h1 className=" text-[30px] lg:text-[52px] font-bold text-white max-w-2xl text-left lg:text-left leading-[1.2] lg:leading-[1.2]">
+          {title}
+        </h1>
+      </div>
+    </div>
+  );
+};
 
-
-        </div>
-    )
-}
-
-export default HeroSectionImg
+export default HeroSectionImg;
