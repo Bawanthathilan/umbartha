bf312dd926ce2a9b9fbc20840c72285781e94e9b
Bawanthathilan <bawantharathnayaka@gmail.com>
2023-12-18 18:51:51 +0530

    fixed book date page issues

--- src/app/dashboard/counsellor/page.tsx
+++ src/app/dashboard/counsellor/page.tsx
@@ -1,155 +1,164 @@
-"use client"
-import React from 'react'
-import PatientCard from '@/components/dashboard/common/patientCard';
-import Image from 'next/image'
-import manisha from '@/assets/home/aboutus/manisha.png'
-import Calender from '@/components/dashboard/common/Calender';
-import DateChip from '@/components/dashboard/common/DateChip';
-import { useRouter } from 'next/navigation';
+"use client";
+import React from "react";
+import PatientCard from "@/components/dashboard/common/patientCard";
+import Image from "next/image";
+import manisha from "@/assets/home/aboutus/people2.png";
+import Calender from "@/components/dashboard/common/Calender";
+import DateChip from "@/components/dashboard/common/DateChip";
+import { useRouter } from "next/navigation";
 
-import { stepCountIncrease } from '@/app/dashboard/reducer/index'
-import { useAppDispatch, useAppSelector } from '@/hooks/reduxHooks';
-
-
+import { stepCountIncrease } from "@/app/dashboard/reducer/index";
+import { useAppDispatch, useAppSelector } from "@/hooks/reduxHooks";
 
 const Page = () => {
-    const dispatch = useAppDispatch();
-    const router = useRouter();
-    const countNo = useAppSelector((state) => state.dashboard.stepCount);
+  const dispatch = useAppDispatch();
+  const router = useRouter();
+  const countNo = useAppSelector((state) => state.dashboard.stepCount);
 
-    const ProceedAction = () => {
-        dispatch(stepCountIncrease(2))
-        if (countNo === 2) {
-            router.push('/dashboard/otp-verification')
-        }
-        return
+  const ProceedAction = () => {
+    dispatch(stepCountIncrease(2));
+    if (countNo === 2) {
+      router.push("/dashboard/otp-verification");
     }
+    return;
+  };
 
-    return (
-        <div className='flex flex-col md:flex-row gap-6 w-full'>
-            <div className="left flex w-full md:w-1/2 flex-col gap-5">
-                <div className="search">
-                    <input type="text" placeholder='Search' className='w-full rounded-[40px] py-3 px-4 border-2 border-[#ECECEC;]' />
-                </div>
-                <PatientCard
-                    img={manisha}
-                    name='Manisha Billimoria'
-                    designation='Counsellor & Therapist'
-                    year={5}
-                />
-                <PatientCard
-                    img={manisha}
-                    name='Manisha Billimoria'
-                    designation='Counsellor & Therapist'
-                    year={5}
-                />
-                <PatientCard
-                    img={manisha}
-                    name='Manisha Billimoria'
-                    designation='Counsellor & Therapist'
-                    year={5}
-                />
-                <PatientCard
-                    img={manisha}
-                    name='Manisha Billimoria'
-                    designation='Counsellor & Therapist'
-                    year={5}
-                />
-                <PatientCard
-                    img={manisha}
-                    name='Manisha Billimoria'
-                    designation='Counsellor & Therapist'
-                    year={5}
-                />
-            </div>
-
-            <div className="right w-full flex flex-col gap-10">
-                <div className='flex group flex-row  gap-5 items-center justify-between  text-black ' >
-
-                    <div className='flex gap-5 items-center'>
-                        <Image src={manisha} alt='manisha' width={60} height={60} className='rounded-full' />
-
-                        <div className="rigth flex flex-col gap-0 ">
-                            <h4 className='text-[20px] font-semibold'>Manisha Billimoria</h4>
-                            <p className='text-sm font-medium'>Counsellor & Therapist</p>
-                        </div>
-                    </div>
-
-
-                    <button
-                        onClick={ProceedAction}
-                        className='bg-black py-[17px] px-10  text-white text-sm font-medium rounded-xl'
-
-                    >Proceed</button>
-                </div>
-
-                <div className="flex flex-col gap-5">
-
-                    <div className="title">
-                        <h4 className='text-[#666] text-sm font-semibold uppercase'>Choose type</h4>
-                    </div>
-                    <div className=' flex flex-col md:flex-row gap-5'>
-                        <div className="flex w-full cursor-pointer flex-row gap-5 px-7 py-7 border-2 border-[#ECECEC] hover:border-[#26AF9F] rounded-xl bg-white hover:bg-[#F0FFFD] text-black hover:text-green-theme ">
-                            <div className="flex">
-                                <div className="cont">
-                                    <h4 className=' text-lg font-semibold'>Online Meeting</h4>
-                                    <p className=' text-[10px] font-medium'>Via Zoom</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="flex w-full cursor-pointer flex-row gap-5 px-7 py-7 border-2 border-[#ECECEC] hover:border-[#26AF9F] rounded-xl bg-white hover:bg-[#F0FFFD] text-black hover:text-green-theme ">
-                            <div className="flex">
-                                <div className="cont">
-                                    <h4 className=' text-lg font-semibold'>Online Meeting</h4>
-                                    <p className=' text-[10px] font-medium'>Via Zoom</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-
-                <div className="div flex flex-col md:flex-row gap-5">
-
-                    <div className="calender">
-                        <div className="title">
-                            <h4 className='text-[#666] text-sm font-semibold uppercase'>Select Date and Time</h4>
-                        </div>
-                        <Calender />
-                    </div>
-
-
-                    <div className="flex flex-col w-full gap-5">
-                        <div className="title">
-                            <h4 className='text-black text-sm font-semibold uppercase'>Wednesday, November 1</h4>
-                        </div>
-                        <div className="grid grid-cols-2 gap-5">
-                            <DateChip
-                                date='09:00'
-                            />
-                            <DateChip
-                                date='09:00'
-                            />
-                            <DateChip
-                                date='09:00'
-                            />
-                            <DateChip
-                                date='09:00'
-                            />
-                            <DateChip
-                                date='09:00'
-                            />
-                            <DateChip
-                                date='09:00'
-                            />
-
-                        </div>
-                    </div>
-                </div>
-
-            </div>
+  return (
+    <div>
+      <div>
+        <h3 className=" text-[28px] font-bold text-left">
+          Select Counsellor and Date
+        </h3>
+      </div>
+      <div className="flex flex-col md:flex-row gap-6 w-full mt-[30px]">
+        <div className="left flex w-full md:w-1/2 flex-col gap-5">
+          <div className="flex flex-col">
+            <h3 className=" text-base font-semibold text-[#666] uppercase">
+              Select the Counsellor
+            </h3>
+          </div>
+          <div className="search">
+            <input
+              type="text"
+              placeholder="Search"
+              className="w-full rounded-[40px] py-3 px-4 border-2 border-[#ECECEC;]"
+            />
+          </div>
+          <PatientCard
+            img={manisha}
+            name="Manisha Billimoria"
+            designation="Counsellor & Therapist"
+            year={5}
+          />
+          <PatientCard
+            img={manisha}
+            name="Manisha Billimoria"
+            designation="Counsellor & Therapist"
+            year={5}
+          />
+          <PatientCard
+            img={manisha}
+            name="Manisha Billimoria"
+            designation="Counsellor & Therapist"
+            year={5}
+          />
+          <PatientCard
+            img={manisha}
+            name="Manisha Billimoria"
+            designation="Counsellor & Therapist"
+            year={5}
+          />
+          <PatientCard
+            img={manisha}
+            name="Manisha Billimoria"
+            designation="Counsellor & Therapist"
+            year={5}
+          />
         </div>
-    )
-}
 
-export default Page
+        <div className="right w-full flex flex-col gap-10">
+          <div className="flex group flex-row  gap-5 items-center justify-between  text-black ">
+            <div className="flex gap-5 items-center">
+              <Image
+                src={manisha}
+                alt="manisha"
+                width={60}
+                height={60}
+                className="rounded-full"
+              />
+
+              <div className="rigth flex flex-col gap-0 ">
+                <h4 className="text-[20px] font-semibold">
+                  Manisha Billimoria
+                </h4>
+                <p className="text-sm font-medium">Counsellor & Therapist</p>
+              </div>
+            </div>
+
+            <button
+              onClick={ProceedAction}
+              className="bg-black py-[17px] px-10  text-white text-sm font-medium rounded-xl"
+            >
+              Proceed
+            </button>
+          </div>
+
+          <div className="flex flex-col gap-5">
+            <div className="title">
+              <h4 className="text-[#666] text-sm font-semibold uppercase">
+                Choose type
+              </h4>
+            </div>
+            <div className=" flex flex-col md:flex-row gap-3">
+              <div className="flex w-full cursor-pointer flex-row gap-5 px-7 py-7 border-2 border-[#ECECEC] hover:border-[#26AF9F] rounded-xl bg-white hover:bg-[#F0FFFD] text-black hover:text-green-theme ">
+                <div className="flex">
+                  <div className="cont">
+                    <h4 className=" text-lg font-semibold">Online Meeting</h4>
+                    <p className=" text-[10px] font-medium">Via Zoom</p>
+                  </div>
+                </div>
+              </div>
+              <div className="flex w-full cursor-pointer flex-row gap-5 px-7 py-7 border-2 border-[#ECECEC] hover:border-[#26AF9F] rounded-xl bg-white hover:bg-[#F0FFFD] text-black hover:text-green-theme ">
+                <div className="flex">
+                  <div className="cont">
+                    <h4 className=" text-lg font-semibold">Online Meeting</h4>
+                    <p className=" text-[10px] font-medium">Via Zoom</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="div flex flex-col md:flex-row gap-5">
+            <div className="calender">
+              <div className="title">
+                <h4 className="text-[#666] text-sm font-semibold uppercase">
+                  Select Date and Time
+                </h4>
+              </div>
+              <Calender />
+            </div>
+
+            <div className="flex flex-col w-full gap-5">
+              <div className="title">
+                <h4 className="text-black text-sm font-semibold uppercase">
+                  Wednesday, November 1
+                </h4>
+              </div>
+              <div className="grid grid-cols-2 gap-[10px]">
+                <DateChip date="09:00" />
+                <DateChip date="09:00" />
+                <DateChip date="09:00" />
+                <DateChip date="09:00" />
+                <DateChip date="09:00" />
+                <DateChip date="09:00" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
--- src/components/dashboard/common/patientCard/index.tsx
+++ src/components/dashboard/common/patientCard/index.tsx
@@ -1,34 +1,34 @@
-import React from 'react'
-import Image from 'next/image'
-
-
+import React from "react";
+import Image from "next/image";
 
 interface PatientCardProps {
-    img: any,
-    name: string,
-    designation: string
-    year: number
+  img: any;
+  name: string;
+  designation: string;
+  year: number;
 }
 
-const PatientCard = ({
-    img,
-    name,
-    designation,
-    year
-}: PatientCardProps) => {
-    return (
-        <div className='flex group flex-row py-3 gap-5 items-center justify-start px-4 border-2 border-[#ECECEC;] rounded-xl hover:bg-green-theme cursor-pointer text-black hover:text-white' >
+const PatientCard = ({ img, name, designation, year }: PatientCardProps) => {
+  return (
+    <div className="flex group flex-row py-3 gap-5 items-center justify-start px-4 border-2 border-[#ECECEC;] rounded-xl hover:bg-green-theme cursor-pointer text-black hover:text-white">
+      <Image
+        src={img}
+        alt="manisha"
+        width={60}
+        height={60}
+        className="rounded-full"
+      />
 
-            <Image src={img} alt='manisha' width={60} height={60} className='rounded-full' />
+      <div className="rigth flex flex-col gap-0 ">
+        <h4 className=" text-base md:text-[20px] font-semibold text-green-theme group-hover:text-white">
+          {name}
+        </h4>
+        <p className=" text-sm font-medium">{designation}</p>
+      </div>
 
-            <div className="rigth flex flex-col gap-0 ">
-                <h4 className=' text-base md:text-[20px] font-semibold'>{name}</h4>
-                <p className=' text-sm font-medium'>{designation}</p>
-            </div>
+      <span className="text-sm font-medium">{year}yr</span>
+    </div>
+  );
+};
 
-            <span className='text-sm font-medium'>{year}yr</span>
-        </div>
-    )
-}
-
-export default PatientCard
+export default PatientCard;
