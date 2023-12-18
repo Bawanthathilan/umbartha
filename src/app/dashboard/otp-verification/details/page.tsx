4982b3e3fcfa9ac810191cc0c3dd86c626a54eb6
Bawanthathilan <bawantharathnayaka@gmail.com>
2023-12-18 21:10:10 +0530

    add otp inputs

--- src/app/dashboard/otp-verification/details/page.tsx
+++ src/app/dashboard/otp-verification/details/page.tsx
@@ -1,32 +1,51 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
-    return (
-        <div className='flex flex-col gap-5'>
-            <div className="title">
-                <h3 className=' text-[28px] font-bold'>Appointment Details</h3>
+  return (
+    <div className="flex flex-col gap-5">
+      <div className="title">
+        <h3 className=" text-[28px] font-bold">Appointment Details</h3>
+      </div>
 
-            </div>
-
-            <div className="form flex flex-col gap-5">
-                <div className="flex flex-col md:flex-row gap-5">
-                    <input type="text" placeholder='Enter yor name' className=' w-full rounded-xl py-3 px-4 border-2 border-[#ECECEC;]' />
-                    <input type="text" placeholder='Enter yor age' className=' w-full md:w-1/2 rounded-xl py-3 px-4 border-2 border-[#ECECEC;]' />
-                </div>
-                <input type="text" placeholder='Enter yor email' className='w-full rounded-xl py-3 px-4 border-2 border-[#ECECEC;]' />
-                <input type="text" placeholder='Phone' className='w-full rounded-xl py-3 px-4 border-2 border-[#ECECEC;]' />
-                <textarea placeholder='Your comment' className='w-full rounded-xl py-3 px-4 border-2 border-[#ECECEC;]' />
-
-
-                <div className="flex flex-row items-end justify-end">
-                    <div className="flex flex-row gap-5 font-semibold text-base">
-                        <button>Back</button>
-                        <button className='bg-[#272727] rounded-xl py-[17px] px-[40px] text-white' >Confirm Details</button>
-                    </div>
-                </div>
-            </div>
+      <div className="form flex flex-col gap-5">
+        <div className="flex flex-col md:flex-row gap-5">
+          <input
+            type="text"
+            placeholder="Enter yor name"
+            className=" w-full rounded-xl py-3 px-4 border-2 border-[#ECECEC;]"
+          />
+          <input
+            type="text"
+            placeholder="Enter yor age"
+            className=" w-full md:w-1/2 rounded-xl py-3 px-4 border-2 border-[#ECECEC;]"
+          />
         </div>
-    )
-}
+        <input
+          type="text"
+          placeholder="Enter yor email"
+          className="w-full rounded-xl py-3 px-4 border-2 border-[#ECECEC;]"
+        />
+        <input
+          type="text"
+          placeholder="Phone"
+          className="w-full rounded-xl py-3 px-4 border-2 border-[#ECECEC;]"
+        />
+        <textarea
+          placeholder="Your comment"
+          className="w-full rounded-xl py-3 px-4 border-2 border-[#ECECEC;]"
+        />
 
-export default page
+        <div className="flex flex-row items-end justify-end">
+          <div className="flex flex-row gap-5 font-semibold text-base">
+            <button>Back</button>
+            <button className="bg-[#272727] rounded-xl py-[17px] px-[40px] text-white">
+              Confirm Details
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
