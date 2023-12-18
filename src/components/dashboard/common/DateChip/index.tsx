392c8d4c02ba5ea5b9d34bf698a8ff978d05628d
Bawanthathilan <bawantharathnayaka@gmail.com>
2023-12-18 23:23:53 +0530

    fixed date chip

--- src/components/dashboard/common/DateChip/index.tsx
+++ src/components/dashboard/common/DateChip/index.tsx
@@ -1,23 +1,18 @@
-import React from 'react'
+import React from "react";
 
 interface DateChipProps {
-    date: string;
-    onClick?: void
+  date: string;
+  onClick?: void;
 }
 
-const DateChip = ({
-    date,
-    onClick
-}: DateChipProps) => {
-    return (
-        <div
+const DateChip = ({ date, onClick }: DateChipProps) => {
+  return (
+    <div className="flex w-full cursor-pointer flex-row gap-5 px-7 py-3 border-2 border-[#ECECEC] hover:border-[#26AF9F] rounded-xl bg-white hover:bg-[#F0FFFD] text-black hover:text-green-theme ">
+      <div className="flex items-center justify-center w-full">
+        <h4 className=" text-sm font-semibold">{date}</h4>
+      </div>
+    </div>
+  );
+};
 
-            className="flex w-full cursor-pointer flex-row gap-5 px-7 py-3 border-2 border-[#ECECEC] hover:border-[#26AF9F] rounded-xl bg-white hover:bg-[#F0FFFD] text-black hover:text-green-theme ">
-            <div className="flex">
-                <h4 className=' text-lg font-semibold'>{date}</h4>
-            </div>
-        </div>
-    )
-}
-
-export default DateChip
+export default DateChip;
